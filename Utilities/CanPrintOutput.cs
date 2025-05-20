81605d2d21fbfdf735d764489762c3ca3d632282
Taras Zotkin <taras.zotkin@jag.se>
2025-05-20 16:36:02 +0200

    Option WIP

---
+++ Utilities/CanPrintOutput.cs
@@ -0,0 +1,12 @@
+﻿using Xunit.Abstractions;
+
+namespace LanguageExt.Introduction.Utilities;
+
+public abstract class CanPrintOutput
+{
+    private readonly ITestOutputHelper _output;
+
+    public CanPrintOutput(ITestOutputHelper output) => _output = output;
+
+    protected void WriteLine(object? value = null) => _output.WriteLine(value?.ToString() ?? string.Empty);
+}
---
+++ Utilities/OptionalInlineDataAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Xunit.Sdk;
+
+namespace LanguageExt.Introduction.Utilities;
+
+public sealed class OptionalInlineDataAttribute : DataAttribute
+{
+    private readonly object[] _data;
+    
+    public OptionalInlineDataAttribute(params string?[] data) => _data = data
+        .Select(Prelude.Optional)
+        .Cast<object>()
+        .ToArray();
+    
+    public override IEnumerable<object[]> GetData(MethodInfo testMethod) => [_data];
+}
